401e60c2fba73a0907f406454d68367e7f9e93b4
CorianiAmedeo <amedeocoriani@gmail.com>
2025-01-14 21:40:23 +0100

    Update to various files and testing of rive app

--- CorianiAmedeo/main.js
+++ CorianiAmedeo/main.js
@@ -1,15 +1,16 @@
 import * as THREE from 'three';
+import * as rive from "@rive-app/canvas";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer( { alpha: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+const material = new THREE.MeshBasicMaterial( { color: 0xfff000 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -23,3 +24,17 @@ function animate() {
 	renderer.render( scene, camera );
 
 }
+
+
+const r = new rive.Rive({
+    src: "https://cdn.rive.app/animations/vehicles.riv",
+    // OR the path to a discoverable and public Rive asset
+    // src: '/public/example.riv',
+    canvas: document.getElementById("canvas"),
+    autoplay: true,
+    // artboard: "Arboard", // Optional. If not supplied the default is selected
+    stateMachines: "bumpy",
+    onLoad: () => {
+      r.resizeDrawingSurfaceToCanvas();
+    },
+});
