5ef834427f8eaf456c21d5d12323b3b149d2dfa8
CorianiAmedeo <amedeocoriani@gmail.com>
2025-04-20 01:02:20 +0200

    Started building CSS and first UI elements. Wip.

--- CorianiAmedeo/main.js
+++ CorianiAmedeo/main.js
@@ -1,24 +1,13 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    var delta = 10;
+    if(Math.abs(prevScrollpos-currentScrollPos)>delta) {
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("resume-cta").style.top = "48px";
+        } else {
+            document.getElementById("resume-cta").style.top = "1px";
+        }
+    }
+  prevScrollpos = currentScrollPos;
+} 
